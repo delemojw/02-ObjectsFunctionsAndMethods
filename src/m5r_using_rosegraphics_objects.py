b216b2b0a43f1ebdd947a9ce27df06c92d0a6874
delemojw <delemojw@rose-hulman.edu>
2018-03-07 09:38:52 -0500

    Answered questions for TODO 2

--- src/m5r_using_rosegraphics_objects.py
+++ src/m5r_using_rosegraphics_objects.py
@@ -10,56 +10,57 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   RUN this program.  Then answer the following,
 #     GETTING HELP AS NEED! (Ask questions!!!)
 #
 #     a. For the RoseGraphics coordinate system:
 #
 #        -- Where is the (0, 0) point on the screen?
-#              WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#            The center of the screen
 #
 #        -- In what direction on the screen does the positive X-axis point?
-#              WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#              to the right
 #
 #        -- In what direction on the screen does the positive Y-axis point?
-#              WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#              forward
 #
 #     b. Write a line of code that constructs a basic RoseWindow object:
-#            WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#            window = rg.RoseWindow()
 #
 #     c. What is the default height of a RoseWindow?
 #          Type into  main  the code shown in your answer above.  That will
 #          ask PyCharm will help you figure out the answer to this question.
 #          Hint: After you type the   (   in the line of code,
 #          if you wait a moment PyCharm will add the   )   and has a popup.
-#            WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#           300 x 3000
 #
 #     d. Write a line of code that constructs a RoseWindow object whose
 #        height is 100 with any width you choose.
 #           Again try to use PyCharm's hints to help you figure this out.
-#               WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#               window = rg.RoseWindow(600, 100)
 #
 #     e. Use the DOT trick to answer the following:
 #
 #          -- Write the names of two types of graphics objects
 #             that you can construct OTHER than Circle and Point:
-#                WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
-#
+#                rg.SimpleTurtle
+#                draw_square
+#                draw_paintBucket
 #          -- Write the names of three METHODs that Circle objects have:
 #               Hint: Use the circle from the  example3  function below with
 #               the dot trick to let PyCharm help you.
-#                  WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#                  attach, draw, fill_color
 #
 #          -- Write the names of three INSTANCE VARIABLEs
 #             that Circle objects have:
-#                WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#                center point, radius, and window
 #
 #     f. What does a RoseWindow RENDER method do?
-#            WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#            draw all objects attacehed to window
 #
 #     g. When is a RoseWindow close_on_mouse_click method call necessary?  Why?
-#            WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#           no, but it makes it easier to close the window.
 #
 #   ASK QUESTIONS ** NOW ** if you do not understand how the
 #     RoseGraphics graphics system works.
@@ -80,6 +81,7 @@ def main():
       -- applying METHODS to them, and
       -- accessing their DATA via INSTANCE VARIABLES
     """
+    #window = rg.RoseWindow()
     example1()
     example2()
     example3()
